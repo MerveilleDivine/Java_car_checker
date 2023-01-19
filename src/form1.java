b82cebe9b0e989517853f34fda276482fc8dacf0
vinemuganguzi <mervinemuganguao@gmail.com>
2023-01-19 11:57:45 +0200

    last commit. the code is working properly

--- src/form1.java
+++ src/form1.java
@@ -1,2 +1,250 @@
-package PACKAGE_NAME;public class form1 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class form1 extends JFrame implements ActionListener {
+    JLabel info = new JLabel("Enter the Car's information", JLabel.CENTER);
+    JLabel Brand = new JLabel("Choose the Brand: ");
+    JComboBox Choose = new JComboBox();
+    JLabel GearBox = new JLabel("Choose the GearBox: ");
+    JRadioButton Manual = new JRadioButton("Manual",false);
+    JRadioButton Auto = new JRadioButton("Automatic",false);
+    ButtonGroup RGroup = new ButtonGroup();
+    JLabel Model = new JLabel("Enter the Model: ");
+    JTextField textModel = new JTextField(10);
+    JLabel Year = new JLabel("Enter the Year: ");
+    JTextField textYear = new JTextField(10);
+    JLabel Speed = new JLabel("Enter the Max Speed: ");
+    JTextField textSpeed = new JTextField(10);
+    JLabel Color = new JLabel("Enter the Color: ");
+    JTextField textColor = new JTextField(10);
+
+    JButton Add = new JButton("Add Car to the File");
+    JButton Open = new JButton("Open the Search Form");
+
+    public form1(){
+        setTitle("Car info");
+        setSize(600,400);
+        //setLayout(new FlowLayout());
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Choose.addItem("Choose");
+        Choose.addItem("Mercedes");
+        Choose.addItem("BMW");
+        Choose.addItem("HONDA");
+        Choose.addItem("Mazda");
+        Choose.addItem("Suzuki");
+        Choose.addItem("Hyundai");
+
+        add(info);
+        add(Brand);
+        add(Choose);
+        add(GearBox);
+
+        this.RGroup.add(Manual);
+        this.RGroup.add(Auto);
+
+        add(Manual);
+        add(Auto);
+        add(Model);
+        add(textModel);
+        add(Year);
+        add(textYear);
+        add(Speed);
+        add(textSpeed);
+        add(Color);
+        add(textColor);
+        add(Add);
+        add(Open);
+
+        Add.setBackground(java.awt.Color.GREEN);
+        Open.setBackground(java.awt.Color.GREEN);
+        Add.setOpaque(true);
+        Open.setOpaque(true);
+
+        Add.addActionListener(this);
+        Open.addActionListener(this);
+
+        info.setFont(new Font("Arial", Font.BOLD, 16));
+        info.setHorizontalAlignment(JLabel.RIGHT);
+        info.setVerticalAlignment(JLabel.TOP);
+        info .setForeground(java.awt.Color.BLUE);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.insets = new Insets(5, 5, 5, 5);
+        gc.anchor = GridBagConstraints.WEST;
+
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(Brand, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(Choose, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 2;
+        add(GearBox, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 2;
+        add(Manual, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 2;
+        add(Auto, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 3;
+        add(Model, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 3;
+        add(textModel, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 4;
+        add(Year, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 4;
+        add(textYear, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 5;
+        add(Speed, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 5;
+        add(textSpeed, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 6;
+        add(Color, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 6;
+        add(textColor, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 8;
+        add(Add, gc);
+
+        gc.gridx =1;
+        gc.gridy = 9;
+        add(Open, gc);
+
+
+        setVisible(true);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String brand = (String) Choose.getSelectedItem();
+        String gearbox;
+        if (Manual.isSelected()) {
+            gearbox = "Manual";
+        } else if (Auto.isSelected()){
+            gearbox = "Automatic";
+        }else {
+        JOptionPane.showMessageDialog(null,"Please select a gearbox (Manual or Automatic)");
+        return;
+        }
+        String model = textModel.getText();
+        String year = textYear.getText();
+        String speed = textSpeed.getText();
+        String color = textColor.getText();
+
+        String carInfo = brand + " " + gearbox + " " + model + " " + year + " " + speed + " " + color + "\n";
+
+        if (e.getSource() == Add) {
+            try {
+                while (brand.equals("Choose") || textModel.getText().isEmpty() || textYear.getText().isEmpty() || textSpeed.getText().isEmpty() || textColor.getText().isEmpty()) {
+                    if(brand.equals("Choose")){
+                        JOptionPane.showMessageDialog(null,"Please select the brand of the car!");
+                    }else {
+                        JOptionPane.showMessageDialog(null,"Please fill in all the fields!");
+                    }
+                    return;
+                }
+
+                    if(!isValidYear(year)){
+                        JOptionPane.showMessageDialog(null,"Please enter a valid year.");
+                        return;
+                    }
+
+
+            File myFile = new File("C:\\Users\\User\\Desktop\\cars.txt");
+            if (!myFile.exists()) {
+                try {
+                    myFile.createNewFile();
+                    FileWriter fw = new FileWriter("C:\\Users\\User\\Desktop\\cars.txt", true);
+                    fw.write(carInfo);
+                    fw.close();
+                    textModel.setText("");
+                    textYear.setText("");
+                    textSpeed.setText("");
+                    textColor.setText("");
+                    Choose.setSelectedIndex(0);
+                    JOptionPane.showMessageDialog(this, "File created and car added in the file");
+                    return;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            } else if (myFile.exists()) {
+                try {
+                    FileWriter fw = new FileWriter("C:\\Users\\User\\Desktop\\cars.txt", true);
+                    fw.write(carInfo);
+                    fw.close();
+                    textModel.setText("");
+                    textYear.setText("");
+                    textSpeed.setText("");
+                    textColor.setText("");
+                    Choose.setSelectedIndex(0);
+
+                    JOptionPane.showMessageDialog(this, "Car added in the file");
+                    return;
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+
+
+        }catch (Exception ex) {
+                JOptionPane.showMessageDialog(this, ex.getMessage());
+            }
+        }
+
+            if (e.getSource()==Open){
+           new form2();
+        }
+        else {
+            dispose();
+
+
+
+
+    }
+}
+    private boolean isValidYear(String year) {
+        try {
+            int yearInt = Integer.parseInt(year);
+            if(yearInt >= 1900 && yearInt <= java.util.Calendar.getInstance().get(java.util.Calendar.YEAR))
+                return true;
+            else
+                return false;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
 }
--- src/form2.java
+++ src/form2.java
@@ -1,2 +1,183 @@
-package PACKAGE_NAME;public class form2 {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class form2 extends JFrame implements ActionListener{
+    JLabel Brand = new JLabel("Brand");
+    JTextField txtBrand = new JTextField(8);
+    JLabel GBox = new JLabel("GearBox");
+    JTextField txtGBox = new JTextField(8);
+    JLabel Mdl = new JLabel("Model");
+    JTextField txtMdl= new JTextField(8);
+    JLabel year = new JLabel("Year");
+    JTextField txtyear = new JTextField(8);
+    JLabel mxSpd = new JLabel("Max Speed");
+    JTextField txtSpd = new JTextField(8);
+    JLabel Col= new JLabel("Color");
+    JTextField txtCol = new JTextField(8);
+
+    JButton Find = new JButton("Find the fastest Car");
+    JButton clr = new JButton("Clear");
+
+    public form2(){
+        setTitle("new Frame");
+        setSize(1000,200);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        add(Brand);
+        add(txtBrand);
+        add(GBox);
+        add(txtGBox);
+        add(Mdl);
+        add(txtMdl);
+        add(year);
+        add(txtyear);
+        add(mxSpd);
+        add(txtSpd);
+        add(Col);
+        add(txtCol);
+
+        add(Find);
+        add(clr);
+        txtCol.setEditable(false);
+        txtBrand.setEditable(false);
+        txtyear .setEditable(false);
+        txtMdl.setEditable(false);
+        txtSpd.setEditable(false);
+        txtGBox.setEditable(false);
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints gc = new GridBagConstraints();
+        gc.insets = new Insets(18, 50, 5, 5);
+        gc.anchor = GridBagConstraints.WEST;
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        add(Brand, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        add(txtBrand, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        add(GBox, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        add(txtGBox, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 0;
+        add(Mdl, gc);
+
+        gc.gridx = 2;
+        gc.gridy = 1;
+        add(txtMdl, gc);
+
+        gc.gridx = 3;
+        gc.gridy = 0;
+        add(year, gc);
+
+        gc.gridx = 3;
+        gc.gridy = 1;
+        add(txtyear, gc);
+
+        gc.gridx = 4;
+        gc.gridy = 0;
+        add(mxSpd, gc);
+
+        gc.gridx = 4;
+        gc.gridy = 1;
+        add(txtSpd, gc);
+
+        gc.gridx = 5;
+        gc.gridy = 0;
+        add(Col, gc);
+
+        gc.gridx = 5;
+        gc.gridy = 1;
+        add(txtCol, gc);
+
+        gc.gridx = 0;
+        gc.gridy = 7;
+        add(Find, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 7;
+        add(clr, gc);
+
+
+        Find.setBackground(Color.GREEN);
+        clr.setBackground(Color.GREEN);
+        Find.setOpaque(true);
+        clr.setOpaque(true);
+
+
+
+        Find.addActionListener(this);
+        clr.addActionListener(this);
+
+        setVisible(true);
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == Find) {
+            String fastestBrand = "";
+            String fastestGearbox = "";
+            String fastestModel = "";
+            String fastestYear = "";
+            String fastestColor = "";
+            int fastestSpeed = 0;
+            try {
+                File myFile = new File("C:\\Users\\User\\Desktop\\cars.txt");
+                Scanner myReader = new Scanner(myFile);
+                while (myReader.hasNextLine()) {
+                    String line = myReader.nextLine();
+                    String[] carInfo = line.split(" ");
+                    System.out.println(Arrays.toString(carInfo)); // print contents of carInfo array
+                    String brand = carInfo[0];
+                    String gearbox = carInfo[1];
+                    String model = carInfo[2];
+                    String year = carInfo[3];
+                    int speed = Integer.parseInt(carInfo[4]);
+                    String color = carInfo[5];
+                    if (speed > fastestSpeed) {
+                        fastestBrand = brand;
+                        fastestGearbox = gearbox;
+                        fastestModel = model;
+                        fastestYear = year;
+                        fastestColor = color;
+                        fastestSpeed = speed;
+                    }
+                }
+                myReader.close();
+
+
+            } catch (IOException e1) {
+                JOptionPane.showMessageDialog(this, "Error reading file");
+            }
+            txtBrand.setText(fastestBrand);
+            txtGBox.setText(fastestGearbox);
+            txtMdl.setText(fastestModel);
+            txtyear.setText(fastestYear);
+            txtCol.setText(fastestColor);
+            txtSpd.setText(Integer.toString(fastestSpeed));
+        }else if (e.getSource()==clr){
+            txtBrand.setText("");
+            txtGBox.setText("");
+            txtMdl.setText("");
+            txtyear.setText("");
+            txtCol.setText("");
+            txtSpd.setText("");
+        }
+    }
 }
